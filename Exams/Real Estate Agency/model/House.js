5ad224983f0c2e9912c015822ecfdec318839ced
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-05-02 20:41:26 +0300

    Create House.js

---
+++ Exams/Real Estate Agency/model/House.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+const schema = new Schema({
+  name: { type: String, required: true },
+  type:{type:String,require:true,enum:['Apartment','Villa','House']},
+  year: { type: Number, required: true },
+  city: { type: String, required: true },
+  homeImage: { type: String, required: true ,match: [/^https?/,'Image must be http or https']},
+  propertyDescription: { type: String, required: true ,maxlength:60},
+  availablePieces: { type: Number, required: true ,min:0,max:10},
+  rentedAHome: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
+  owner: { type: Schema.Types.ObjectId, ref: "User" },
+});
+//min: [6,'The Name should be at least 6 characters']
+//(“Apartment”, “Villa”, “House”) 
+module.exports = model("House", schema);
