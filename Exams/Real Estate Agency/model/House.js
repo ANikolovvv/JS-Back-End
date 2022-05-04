3d6fb48714fcbc568c92f6a6ac20e87f9ebe8abe
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-05-04 20:39:08 +0300

    Update House.js

--- Exams/Real Estate Agency/model/House.js
+++ Exams/Real Estate Agency/model/House.js
@@ -8,7 +8,7 @@ const schema = new Schema({
   homeImage: { type: String, required: true ,match: [/^https?/,'Image must be http or https']},
   propertyDescription: { type: String, required: true ,maxlength:60},
   availablePieces: { type: Number, required: true ,min:0,max:10},
-  rentedAHome: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
+  rentedAHome: [{ type: Schema.Types.String, ref: "User", default: [] }],
   owner: { type: Schema.Types.ObjectId, ref: "User" },
 });
 //min: [6,'The Name should be at least 6 characters']
