5989564ddd3d8f71fd934f4becd73d9d846cb8b3
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-06-03 19:18:47 +0300

    Refactoring part 1

--- Cubicle/controllers/404.js
+++
@@ -1,4 +0,0 @@
-module.exports=(req,res)=>{
-    console.log(404)
-    res.render('404');
-}
---
+++ Cubicle/src/config/datebase.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+const url='mongodb://localhost:27017/softuni-cubicle';
+   
+async function db() {
+    try {
+        await mongoose.connect(url);
+        console.log('DB is working')
+      } catch (err) {
+        console.log(err,'Error in Datebase')
+      }
+}
+module.exports=db
