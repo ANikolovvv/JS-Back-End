aeb580431e9c8405e56109a4c88a1986cf45c12f
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-06-05 11:44:03 +0300

    index.js

---
+++ Cubicle/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const hbs = require("express-handlebars");
+const db = require("./config/datebase");
+const port = 3000;
+const router = require("./config/routes");
+start();
+async function start() {
+  app.engine(
+    "hbs",
+    hbs.engine({
+      extname: "hbs",
+    })
+  );
+  app.set("view engine", "hbs");
+  app.use("/static", express.static("static"));
+  app.use(express.urlencoded({ extended: false }));
+  app.use(router);
+  db()
+
+  app.listen(port,() => console.log(`App is listening to port ${port}`));
+}
