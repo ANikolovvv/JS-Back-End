a28e5b56566fd3a467e23c42c448159b37926108
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-06-25 13:29:52 +0300

    create env

---
+++ Exams/Crypto/config/env.js
@@ -0,0 +1,5 @@
+exports.PORT = 3000;
+exports.SECRET_TOKEN = "Houston, we have a problem (:";
+exports.SESSION_NAME = "Session houston";
+exports.DB_CONNECTION_STRING = "mongodb://localhost:27017/exam";
+exports.SALT_ROUNDS = 10;
